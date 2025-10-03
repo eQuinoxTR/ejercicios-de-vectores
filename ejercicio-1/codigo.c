a5be61a55eb8b5146462b314b242e4b9c74c7159
eQuinoxTR <tiziano313rivadeneira@gmail.com>
2025-10-02 23:39:02 -0300

    agrege los ejercicios que hice hasta ahora

--- ejercicio-1/codigo.c
+++ ejercicio-1/codigo.c
@@ -1,21 +1,8 @@
 #include <stdio.h>
 
-int determinarLongitud(char string[]);
-
 int main() 
 {
-    char string[50];
-    printf("ingresa un texto para determinar su longitud ");
-    scanf("%50s", string);
-    int longitud = determinarLongitud(string);
-    printf("la longitud del texto es de %d, el texto: %s", longitud, string);
+    printf("Hello World");
+    
     return 0;
-}
-
-int determinarLongitud(char string[]) {
-    int i = 0;
-    while(string[i] != '\0') { // huy, cuando termine el vector sali del while
-        i++;
-    }
-    return i;
 }
